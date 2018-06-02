08ec66300a4b64fa8136bf0f66b6598765a03e73
Jim Schaub <jschau@me.com>
2018-06-02 14:35:47 -0500

    Initial Commit

---
+++ BullsEye/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Jim Schaub on 5/26/18.
+//  Copyright © 2018 Jim Schaub. All rights reserved.
+//
+	
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var currentValue: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        print("The value of slider is now: \(slider.value)")
+        currentValue = lroundf(slider.value)
+    }
+    
+    @IBAction func showAlert() {
+        
+        let message = "The value of the slider is: \(currentValue)"
+        
+        let alert = UIAlertController(title: "Hello World!", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+
+
+}
+
